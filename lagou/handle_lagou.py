aac312f7436b3d56a24f424214402637f847f2f4
exia <someone741@qq.com>
2019-07-10 20:48:27 +0800

    更新拉取数据

--- lagou/handle_lagou.py
+++ lagou/handle_lagou.py
@@ -4,6 +4,7 @@ import requests
 class handle_lagou():
     # 初始化拉勾网爬虫
     def __init__(self):
+        # 使用session保存cookies信息
         self.session=requests.session()
         self.header={
             'User-Agent': 'Mozilla / 5.0(Windows NT 10.0;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 75.0.3770.100Safari / 537.36'
@@ -16,15 +17,47 @@ class handle_lagou():
         city_url = "https://www.lagou.com/jobs/allCity.html"
         city_result = self.handle_request(method="GET", url=city_url)
         self.city_list=city_comp.findall(city_result)
+        self.session.cookies.clear()
+
+    def handle_city_job(self, city):
+        first_request_url = "https://www.lagou.com/jobs/list_python?city=%s&cl=false&fromSearch=true&labelWords=&suginput="%city
+        first_response = self.handle_request(method="GET", url=first_request_url)
+        total_page_re = re.compile(r'class="span\stotalNum">(\d+)</span>')
+        try:
+            total_page = total_page_re.search(first_response).group(1)
+        # 由于没有岗位信息造成的exception
+        except:
+            return
+        else:
+            for i in range(1,int(total_page)+1):
+                data = {
+                    "pn":i,
+                    "kd":"python",
+                }
+                page_url = "https://www.lagou.com/jobs/pisitionAjax.json?city=%s&needAddtionalResult=false"%city
+                refer_url = "https://www.lagou.com/jobs/list_python?city=%s&cl=false&fromSearch=true&labelWords=&suginput="%city
+                self.header['Referer'] = refer_url.encode()
+                response = self.handle_request(method="POST",url=page_url,data=data)
+                print(response)
 
     # 定义获取方法
     def handle_request(self, method, url, data=None, info=None):
         if method=="GET":
             response = self.session.get(url=url, headers=self.header)
+
+        elif method=="POST":
+            response=self.session.post(url=url, headers=self.header, data=data)
+
+        response.encoding = 'utf-8'
         return response.text
 
 
 if __name__ == '__main__':
     lagou = handle_lagou()
+    # 获取所有城市
     lagou.handle_city()
     print(lagou.city_list)
+    for city in lagou.city_list:
+        lagou.handle_city_job(city)
+        break
+
